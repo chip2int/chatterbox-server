57813a53ff2911408b5e279b6799957e92fa2ce0
Hack Reactor Students <jdtoney@gmail.com>
2013-12-07 17:41:38 -0800

    Impl 3

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,9 +1,12 @@
-/* You should implement your request handler function in this file.
- * And hey! This is already getting passed to http.createServer()
- * in basic-server.js. But it won't work as is.
- * You'll have to figure out a way to export this function from
- * this file and include it in basic-server.js so that it actually works.
- * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+var statusCode;
+
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10,
+  "Content-Type": "application/json"
+};
 
 
 exports.storage = {
@@ -18,67 +21,65 @@ exports.storage = {
   }
 };
 
+var actionListener = {
+  'POST': saveMessage,
+  'GET': sendMessages,
+  'OPTIONS': sendOptions
+};
+
+
+var sendResponse = function (request, obj, callback) {
+
+};
+
+
+var saveMessage = function(request, response){
+  var data = '';
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+
+
+  request.on('end', function(chunk){
+    var temp = JSON.parse(data);
+    temp['createdAt'] = new Date();
+    temp = JSON.stringify(temp);
+    exports.storage.storeData(temp);
+    statusCode = 201;
+    response.writeHead(statusCode, headers);
+    response.end('[' + exports.storage.getData() + ']');
+  });
+
+};
+
+var sendMessages = function(request, response){
+  statusCode = 200;
+  response.writeHead(statusCode, headers);
+  response.end('[' + exports.storage.getData() + ']');
+};
+
+var sendOptions = function(request, response){
+  statusCode = 200;
+  response.writeHead(statusCode, headers);
+  response.end(JSON.stringify(null));
+};
+
+
 
 
 exports.handleRequest = function(request, response) {
 
-  /* the 'request' argument comes from nodes http module. It includes info about the
-  request - such as    URL the browser is requesting. */
-
-  /* Documentation for both request and response can be found at
-   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
-  var statusCode = 200;
   console.log("Serving request type " + request.method + " for url " + request.url);
 
   request.setEncoding('utf8');
 
-  /*
-  var methods = {
-    'POST': function() {
-      request.on('data', function(chatMsg) {
-        var temp = JSON.parse(chatMsg);
-        temp['createdAt'] = new Date();
-        temp = JSON.stringify(temp);
-        exports.storage.storeData(temp);
-        statusCode = 201;
-      }
-
-    }
-  }
-
-  methods[request.method]();
-  */
-
-  if (request.method === 'POST') {
-      request.on('data', function(chatMsg) {
-        var temp = JSON.parse(chatMsg);
-        temp['createdAt'] = new Date();
-        temp = JSON.stringify(temp);
-        exports.storage.storeData(temp);
-        statusCode = 201;
-    });
-  }
-
-
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "application/json";
-
   response.writeHead(statusCode, headers);
 
-
-  response.end('[' + exports.storage.getData() + ']');
-
-
+  actionListener[request.method](request, response);
+  
 };
 
 
-var defaultCorsHeaders = {
-  "access-control-allow-origin": "*",
-  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-  "access-control-allow-headers": "content-type, accept",
-  "access-control-max-age": 10 // Seconds.
-};
 
 
 
